982d22c7704f361d254f62a863cf3d5443815347
toan.tranduc <toan.tranduc@vti.com.vn>
2023-07-17 17:52:13 +0700

    coding ex2

--- src/main/java/com/example/challenge/service/PeopleService.java
+++ src/main/java/com/example/challenge/service/PeopleService.java
@@ -4,9 +4,10 @@ import com.example.challenge.entity.People;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.List;
 
 public interface PeopleService {
 
     Page<People> getAllPeople(Pageable pageable);
+
+    Boolean mergeJson();
 }
